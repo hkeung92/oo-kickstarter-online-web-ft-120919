5faf6f46b6684ec91f98eebacf30696bda339eec
Ho Yin Keung <hykeung92@gmail.com>
2020-01-13 00:00:16 +0000

    Automatically backed up by Learn

---
+++ lib/backer.rb
@@ -0,0 +1,13 @@
+class Backer
+  attr_accessor :name, :backed_projects
+  
+  
+  def initialize(name)
+    @name = name
+    @backed_projects = []
+  end
+  
+  def backer(project)
+    @backed_projects << self
+  end
+end
